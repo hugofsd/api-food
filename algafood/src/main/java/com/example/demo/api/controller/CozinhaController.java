09779b389f19b647a7cd6154fde808d12fababa8
Hugo França <hugo619@outlook.com>
2022-08-09 19:59:27 -0300

    4.13. Implementando content negotiation para retornar JSON ou XML

--- algafood/src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.example.demo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class CozinhaController {
 	private CozinhaRepository cozinhaRepository;
 	
 	// Deu certo apenas dando run do AlgaFoodApplication
-	@GetMapping
+	@GetMapping (produces = MediaType.APPLICATION_JSON_VALUE) // Retorno apenas em json
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
