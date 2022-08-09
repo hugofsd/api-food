7b2143756e59619061cd177d5d50ce1c78235887
Hugo França <hugo619@outlook.com>
2022-08-09 19:19:37 -0300

    4.10. Modelando e requisitando um Collection Resource com GET. Get de cozinhas

--- algafood/src/main/java/com/example/demo/infrastructure/repository/CozinhaRepositoryImpl.java
+++ algafood/src/main/java/com/example/demo/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -1,10 +1,17 @@
 package com.example.demo.infrastructure.repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,5 +53,7 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 		cozinha = buscar(cozinha.getId());
 		manager.remove(cozinha);	
 	}
+
+  // METODOS A+ FORA DO CURSO
 
 }
