eebbc46489d34694bb7a6176225cb268632ff2fb
Hugo França <hugo619@outlook.com>
2022-08-11 16:31:33 -0300

    4.23. Modelando e implementando a inclusão de recursos com POST

--- algafood/src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -8,7 +8,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.model.Cozinha;
@@ -40,6 +43,12 @@ public class CozinhaController {
 
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar( @RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	 
 	//@ResponseStatus(HttpStatus.OK) // Escolher status
 	@GetMapping ("controleStatus/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscarComControleStatus( @PathVariable("cozinhaId") Long cozinhaId) {
