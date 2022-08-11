e52b22ce367d6c2d1f0635a2988ce481dcc005eb
Hugo França <hugo619@outlook.com>
2022-08-11 17:15:15 -0300

    4.25. Modelando e implementando a atualização de recursos com PUT

--- algafood/src/main/java/com/example/demo/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/example/demo/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.example.demo.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,6 +49,32 @@ public class CozinhaController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar( @RequestBody Cozinha cozinha) {
 		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	
+	//ResponseEntity usado pois o obj será tratado
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		
+		//buscar a cozinha
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+		
+		if(cozinhaAtual != null) {
+			//copiar o obj recebido e passar para o obj atual
+		    BeanUtils.copyProperties(cozinha, cozinhaAtual, "id"); 
+		    // em aspas são declaradas as propriedades ignoradas
+			
+		    //salvar cozinha atual
+		    cozinhaRepository.salvar(cozinhaAtual);
+		    
+		    //status ok
+		    return ResponseEntity.ok(cozinhaAtual);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+		
+	
+		
 	}
 	 
 	//@ResponseStatus(HttpStatus.OK) // Escolher status
