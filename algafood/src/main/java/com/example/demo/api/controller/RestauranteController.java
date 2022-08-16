dc4b4eb88675268c6c1feca9cd55011d9b28ad6c
Hugo França <hugo619@outlook.com>
2022-08-16 02:48:05 -0300

    4.31. Desafio: Modelando e implementando a atualização de recursos de restaurantes. PUT detalhando Exception

--- algafood/src/main/java/com/example/demo/api/controller/RestauranteController.java
+++ algafood/src/main/java/com/example/demo/api/controller/RestauranteController.java
@@ -2,18 +2,24 @@ package com.example.demo.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.model.Restaurante;
 import com.example.demo.domain.repository.RestauranteRepository;
-import com.example.demo.domain.service.CadastroCozinhaService;
+import com.example.demo.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -23,7 +29,7 @@ public class RestauranteController {
 	private RestauranteRepository restauranteRepository;
 	
 	@Autowired
-	private CadastroCozinhaService cadastroCozinhaService;
+	private CadastroRestauranteService cadastroRestauranteService;
 	
 	// Deu certo apenas dando run do AlgaFoodApplication
 	@GetMapping (produces = MediaType.APPLICATION_JSON_VALUE) // Retorno apenas em json
@@ -41,6 +47,53 @@ public class RestauranteController {
 			return ResponseEntity.notFound().build();
 		}
 
+	}
+	
+	//ResponseEntity usado para customizar o status com o tipo do corpo <> Restaurante
+	@PostMapping
+	public ResponseEntity<?> adicionar( @RequestBody Restaurante restaurante){
+		try {
+			restaurante = cadastroRestauranteService.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(restaurante);
+		}catch (EntidadeNaoEncontradaException erro) {
+			//badRequest para notificar que alguma coisa incluida foi errada
+			return ResponseEntity.badRequest()
+					.body(erro.getMessage());
+		}
+	
+	}
+	
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar (@PathVariable Long restauranteId, 
+			@RequestBody Restaurante restaurante){
+		
+		try {
+			//buscar o restaurante
+			Restaurante restauranteAutual = restauranteRepository
+			.buscar(restauranteId);
+			
+			if(restauranteAutual != null) {
+				//copiar o obj recebido e passar para o obj atual
+				BeanUtils.copyProperties(restaurante, restauranteAutual, "id");
+	            // em aspas são declaradas as propriedades ignoradas
+				
+			    //salvar cozinha atual
+				restauranteAutual = cadastroRestauranteService.salvar(restauranteAutual);
+				 //status ok
+			    return ResponseEntity.ok(restauranteAutual);
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+			
+		} catch (EntidadeNaoEncontradaException erro) {
+			//badRequest para notificar que alguma coisa incluida foi errada
+			return ResponseEntity.badRequest()
+					.body(erro.getMessage());
+		}
+		
+		
 	}
 	
 
---
+++ algafood/src/main/java/com/example/demo/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,39 @@
+package com.example.demo.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
+import com.example.demo.domain.model.Cozinha;
+import com.example.demo.domain.model.Restaurante;
+import com.example.demo.domain.repository.CozinhaRepository;
+import com.example.demo.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	public Restaurante salvar (Restaurante restaurante) {
+		
+		//buscar id da cozinha atrelada ao restaurante
+		Long cozinhaId = restaurante.getCozinha().getId();
+		
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if(cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+			String.format("Não existe cadastro de cozinha com o codigo %d", cozinhaId));
+			
+		}
+		
+		restaurante.setCozinha(cozinha);
+		
+		return restauranteRepository.salvar(restaurante);
+	}
+	
+}
