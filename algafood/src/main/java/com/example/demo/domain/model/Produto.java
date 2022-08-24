305c8db43be7a5cc8d0fc9f9c458211fbc84fd86
Hugo França <hugo619@outlook.com>
2022-08-24 06:47:52 -0300

    6.7. Desafio: mapeando relacionamento muitos-para-um

---
+++ algafood/src/main/java/com/example/demo/domain/model/Produto.java
@@ -0,0 +1,43 @@
+package com.example.demo.domain.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data // trás getter e satters
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // gerar hascode e equals direto na var 
+public class Produto {
+	
+	@EqualsAndHashCode.Include 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO ENCREMENTO DE ID
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+	
+	@ManyToOne
+	@Column(nullable = false)
+	private Restaurante restaurtante;
+	
+	
+
+}
