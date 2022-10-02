88cfda7355107d41b1da83521bb99ffd18cdb3b7
Windows 10 <Windows 10@DESKTOP-JUILF2A>
2022-10-02 14:18:22 -0300

    8.4. Estendendo ResponseStatusException

--- algafood/src/main/java/com/example/demo/domain/exception/EntidadeNaoEncontradaException.java
+++ algafood/src/main/java/com/example/demo/domain/exception/EntidadeNaoEncontradaException.java
@@ -2,14 +2,24 @@ package com.example.demo.domain.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntidadeNaoEncontradaException extends RuntimeException {
+//@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
 
 	private static final long serialVersionUID = 1L;
 	
+	
+	//recebe status e devolve uma mensagem
+	public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
+		super(status, mensagem);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
 	public EntidadeNaoEncontradaException(String mensagem) {
-		super(mensagem);
+		this(HttpStatus.NOT_FOUND, mensagem );
 	}
 
 }
