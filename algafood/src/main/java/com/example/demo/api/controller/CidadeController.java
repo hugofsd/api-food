7c9c94cf5f984213c6f5339085d500d16bde93af
Hugo França <hugo619@outlook.com>
2023-01-08 00:19:54 -0300

    Correção de import 8.18

--- algafood/src/main/java/com/example/demo/api/controller/CidadeController.java
+++ algafood/src/main/java/com/example/demo/api/controller/CidadeController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.api.exceptionhandler.Problema;
+import com.example.demo.api.exceptionhandler.Problem;
 import com.example.demo.domain.exception.EntidadeEmUsoException;
 import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.exception.EstadoNaoEncontradaException;
