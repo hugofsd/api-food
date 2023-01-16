e85049a27a1972dffd01350290f708a344b37531
Hugo França <hugo619@outlook.com>
2023-01-16 00:01:31 -0300

    9.2. Adicionando constraints e validando no controller com @Valid

--- algafood/src/main/java/com/example/demo/api/controller/RestauranteController.java
+++ algafood/src/main/java/com/example/demo/api/controller/RestauranteController.java
@@ -1,18 +1,14 @@
 package com.example.demo.api.controller;
 
-import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.ReflectionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,7 +18,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.exception.CozinhaNaoEncontradaException;
-import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.exception.NegocioException;
 import com.example.demo.domain.model.Restaurante;
 import com.example.demo.domain.repository.RestauranteRepository;
@@ -50,8 +45,10 @@ public class RestauranteController {
 	}
 	
 	//ResponseEntity usado para customizar o status com o tipo do corpo <> Restaurante
+	//@Valid antes de executar já é feita a validação, pegando o notnull do model
 	@PostMapping
-	public Restaurante adicionar( @RequestBody Restaurante restaurante){
+	@ResponseStatus(HttpStatus.CREATED)
+	public Restaurante adicionar( @RequestBody @Valid Restaurante restaurante){
 		try {
 			return cadastroRestauranteService.salvar(restaurante);
 		} catch (CozinhaNaoEncontradaException e) {
