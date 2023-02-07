98591c49b6aa2b83277991bba118a06372e6d902
Hugo França <hugo619@outlook.com>
2023-02-07 09:46:33 -0300

    Limpando class teste

--- algafood/src/test/java/com/example/demo/CadastroCozinhaIT.java
+++ algafood/src/test/java/com/example/demo/CadastroCozinhaIT.java
@@ -1,38 +1,16 @@
 package com.example.demo;
 
-import static io.restassured.RestAssured.given;
-
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import org.junit.jupiter.api.Test; // solução de tutor
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // para levantar servidor web
+
+@SpringBootTest
 public class CadastroCozinhaIT {
 
-	@LocalServerPort
-	private int port; // recebe a nova porta para testes
-	
-	@Test
-	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
-		given()
-			.basePath("/cozinhas")
-			.port(port)
-			.accept(ContentType.JSON)
-		.when()
-			.get()
-		.then()
-			.statusCode(HttpStatus.OK.value());
-	}
-	
+	//CONTINUAR
 
 }
